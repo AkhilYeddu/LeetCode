d424be4b8fe0670fecdb102463c2d7fbc25cb807
Akhil Yeddu <148748899+AkhilYeddu@users.noreply.github.com>
2026-07-09 17:50:29 +0530

    Time: 3 ms (45.91%) | Memory: 23.5 MB (18.25%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -3,9 +3,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        p1 = 0
+        p1 = 0 
         p2 = len(s) - 1
-        while p1 < p2:
+        while p1 < p2 :
             temp = s[p1]
             s[p1] = s[p2]
             s[p2] = temp
